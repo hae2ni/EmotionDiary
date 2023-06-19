2cf4dc48b829fbecefed40f97a7a6eefb753be6d
HAE2NI <hae0_02ni@ewhain.net>
2023-06-19 21:46:33 +0900

    ✨ reducer

--- EmotionDiary/src/App.jsx
+++ EmotionDiary/src/App.jsx
@@ -4,17 +4,68 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import happy from "./assets/emotion1.png";
-import MyHeader from "./components/MyHeader";
-import MyButton from "./components/MyButton";
+import { useReducer, useRef } from "react";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targedId });
+  };
+
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader headText={"App"} leftChild={<MyButton text={"왼쪽버튼"} />} />
-        <h2>App.js</h2>
-        <img src={happy} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
