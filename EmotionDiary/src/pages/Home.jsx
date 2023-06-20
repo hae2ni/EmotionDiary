22bfba402e999bfce3dfe11124570852d8724d37
HAE2NI <hae0_02ni@ewhain.net>
2023-06-20 18:46:39 +0900

    ✨Home

--- EmotionDiary/src/pages/Home.jsx
+++ EmotionDiary/src/pages/Home.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { DiaryStateContext } from "../App";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const dirayList = useContext(DiaryStateContext);
 
-export default Home
+  const [curDate, setCurDate] = useState(new Date());
+  const [data, setData] = useState([]);
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth(+1)}월`;
+  useEffect(() => {
+    if (dirayList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+
+      const LastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        dirayList.filter((it) => firstDay <= it.date && it.date <= LastDay)
+      );
+    }
+  }, [dirayList, curDate]);
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
+      <DiaryList diaryList={data} />
+    </div>
+  );
+};
+
+export default Home;
